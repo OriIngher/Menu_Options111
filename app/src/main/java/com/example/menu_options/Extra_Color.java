bd4a35410b7b8fc425a4bddbcbf92292274cde07
OriIngher <aa9803@bs.amalnet.k12.il>
2019-11-06 10:33:17 +0200

    a

---
+++ app/src/main/java/com/example/menu_options/Extra_Color.java
@@ -0,0 +1,13 @@
+package com.example.menu_options;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Extra_Color extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra__color);
+    }
+}
---
+++ app/src/main/java/com/example/menu_options/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.menu_options;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+Button second_activity;
+LinearLayout ll;
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id =item.getItemId();
+        if(id==R.id.Black)
+        {
+            ll.setBackgroundColor(Color.BLACK);
+        }
+        else
+            if (id==R.id.Blue)
+            {
+                ll.setBackgroundColor(Color.BLUE);
+            }
+            else
+                if (id==R.id.Yelloew)
+                {
+                    ll.setBackgroundColor(Color.YELLOW);
+                }
+
+
+        return true;
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        second_activity= (Button) findViewById(R.id.second_activity);
+        ll = (LinearLayout) findViewById(R.id.ll);
+
+    }
+
+    public void second_activity(View view) {
+        Intent si = new Intent(this,Extra_Color.class);
+        startActivity(si);
+
+    }
+}
